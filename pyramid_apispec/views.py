41c0ed4b12b3cf491c52f2f9e66e120f2637b0e6
Marcin Lulek <info@webreactor.eu>
2021-10-17 14:24:04 +0200

    bump swagger ui to 3.52.4

--- pyramid_apispec/views.py
+++ pyramid_apispec/views.py
@@ -57,7 +57,7 @@ def build_api_explorer_view(
             )
 
         script_callable = getattr(imported_package, callable)
-        version = kwargs.get("ui_version", "3.35.2")
+        version = kwargs.get("ui_version", "3.52.4")
         html = Template(template).safe_substitute(
             swagger_ui_script=script_callable(request, spec_route_name, **kwargs),
             version=version,
